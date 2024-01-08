ea945e831fac8130b95dd50eb29ca13400603fee
Ramraj-Perumal <ramraj.perumal25@gmail.com>
2024-01-08 23:52:54 +0530

    project done

--- src/Context/AppReducer.js
+++ src/Context/AppReducer.js
@@ -1,17 +1,18 @@
-
-export default (state, action) => {
+const appReducer = (state, action) => {
     switch (action.type) {
         case 'Delete_Transaction':
             return {
                 ...state,
                 transactions: state.transactions.filter((transaction) => transaction.id !== action.payload)
-            }
-            case 'Add_Transaction':
+            };
+        case 'Add_Transaction':
             return {
                 ...state,
                 transactions: [action.payload, ...state.transactions]
-            }
+            };
         default:
             return state;
     }
 }
+
+export default appReducer;
